3b7a82eb3413f4a13f5b6fac9bf8dbfddb1954eb
Meghan White <meghanwhite11@gmail.com>
2016-07-14 09:48:14 -0400

    Integers file added

---
+++ 1.10_integers.js
@@ -0,0 +1,21 @@
+//Integers//
+
+//Integers are numbers...you can add, subtract, divide, multiply, etc 
+
+//Operators
+5 + 4    //addition
+25 - 5   //subtraction
+4 * 56   //multiplication
+99 / 3   // division
+
+
+//modulus// this math operator returns the reminder if you divide the second number into the first
+25 % 5   
+16 % 9
+
+//you can also assign integers as variables
+var bill = 25
+var tip = 3
+
+var totalCost = (bill + tip);
+	console.log(totalCost)
