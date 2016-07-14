7458fb2cfdbf2f82acf0b0dc3252b850e4234677
Meghan White <meghanwhite11@gmail.com>
2016-07-14 11:37:11 -0400

    added arrays file

---
+++ 1.70_arrays.js
@@ -0,0 +1,15 @@
+//Arrays
+
+//Arrays are a great way to store a list or group of items
+//They can include strings, or integers, or both
+//Arrays use square brackets []
+//The first entry in an array is at position 0
+
+var friendsCast = ["Monica", "Rachel", "Phoebe", "Joey", "Chandler", "Ross"]
+
+//Accessing items in an array
+
+console.log(friendsCast); //will access the entire array 
+console.log(friendsCast[0]); //will print Monica's name
+console.log(friendsCast.length); //will return the length of the array: 6
+
