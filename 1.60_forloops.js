d3c77e948257944c12a0e5d8f90ae08eadb5c374
Meghan White <meghanwhite11@gmail.com>
2016-07-14 11:24:43 -0400

    added for loops file

---
+++ 1.60_forloops.js
@@ -0,0 +1,20 @@
+//For Loops
+
+//For loops have three parts separated by semi-colons: 
+//1. variable (assigns a starting point)
+//2. conditional 
+//3. increments (these can count up or down)
+//It's typical to use the variable i in for loops
+
+//this for loop will count to 10 by 2s and print 
+for(var i = 0; i <= 10; i+=2){
+	console.log ("number: ", i);
+}
+
+
+
+//this for loop will count backwards from 100 by 5s
+for(var i = 100; i>= 0; i-=5){
+	console.log("number: ", i);
+}
+
