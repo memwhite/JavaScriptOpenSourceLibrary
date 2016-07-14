568c593f1c4e3ec2cf9dda9f86f3954cc48f4369
Meghan White <meghanwhite11@gmail.com>
2016-07-14 10:15:53 -0400

    added boolean file

---
+++ 1.20_booleans.js
@@ -0,0 +1,13 @@
+//Booleans: True/False statements
+
+10 > 2; //returns true
+10 > 100; //returns false
+
+10 === 10; //returns True because 10 is equal to 10
+10 != 10; //returns False because 10 is != (not equal) to 10
+
+// && - this indicates AND
+// || - this indicates OR
+
+23 > 100 && 45 < 100; //this will return false because BOTH expressions have to be true to return true
+23 > 100 || 45 < 100; //this will return true because only one of the expressions have to be true to return true 
