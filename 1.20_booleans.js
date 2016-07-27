570538df013c326bb0fd2c607a4bb3f725b1b313
Meghan White <meghanwhite11@gmail.com>
2016-07-27 16:13:42 -0400

    added things to the boolean file

--- 1.20_booleans.js
+++ 1.20_booleans.js
@@ -6,5 +6,11 @@
 var bettyWhiteAge = 100;
 
 if (bettyWhiteAge > 90){  //this will run and print console.log because it's true
-	console.log("Holy cow, that's old!")
+	console.log("Holy cow, that's old!");
 }
+
+//Here are examples of things that will evaluate to FALSE:
+//false, null, undefined, an empty string '', 0, NaN
+
+//Examples of what will evaluate to TRUE:
+//everything else, including: true, the string "false", any number besides 0, and all objects
