28eda0f529f6f5d7dd7f724ea2892a24d23b11c6
Meghan White <meghanwhite11@gmail.com>
2016-08-01 15:00:15 -0400

    added ternary operator file

---
+++ 1.31_ternary_operator.js
@@ -0,0 +1,15 @@
+//Ternary operators
+
+//evalutes the statement, and if it's true, it returns whatever is on the left side of the colon (:)
+//if it's false, it will return what's on the right side of the colon (:)
+
+var harryPotter = "wizard";
+
+
+//returns: You're a wizard Harry
+harryPotter === "wizard" ? console.log("You're a wizard Harry") : console.log("You're a muggle Harry");
+
+//returns: You're a muggle Harry
+harryPotter === "not a wizard" ? console.log("You're a wizard Harry") : console.log("You're a muggle Harry"); 
+
+
